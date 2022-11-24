5d6e88c1f0fd906c8b284a1eb790e08fd16cb9bc
Hassan Mushtaq <hassanmushtaq524@gmail.com>
2022-11-23 21:58:29 -0500

    solution - slow

--- main.cpp
+++ main.cpp
@@ -3,35 +3,42 @@
 #include <math.h>
 #include <fstream>
 #include <dirent.h>
+#include <float.h>
 using namespace std;
 
 
+// 
+// Output: returns the distance between two nodes
+double nodeDistance(int node1, int node2, vector<double> &x, vector<double> &y) {
+    // cout << x[node1] << " " << x[node2] << endl;
+    // cout << y[node1] << " " << y[node2] << endl;
+    double distance = sqrt(pow(x[node1] - x[node2], 2) + pow(y[node1] - y[node2], 2));
+    return distance;
+}
 
 // Input: 
 // Output: returns the minimum cost to traverse the nodes
-double travelSales(int node, vector<double> &x, vector<double> &y, vector<int> route) {
-    route.push_back(node);
-    if (route.size() == x.size()) {
-        double distance = sqrt(pow(x[node] - x[0], 2) + pow(y[node] - y[0], 2));
-        return distance;
-    }
-    int n = x.size();
-    int minNode;
-    double minCost = 99999;
-    double cost;
-    for (int i = 0; i < n; i++) {
-        // node has not been explored
-        if (find(route.begin(), route.end(), i) == route.end()) {
-            cost = travelSales(i, x, y, route);
-            if (cost < minCost) {
-                minCost = cost;
-                minNode = i;
-            }
+double travelSales(int pos, double currentLength, vector<double> &x, vector<double> &y, vector<int> &route, double &minCost) {
+   int n = route.size();
+   double newLength;
+   if (pos == n - 1) {
+    minCost = min(minCost,  currentLength + nodeDistance(route[pos], route[0], x, y));
+   } else {
+    for (int i = pos + 1; i < n; i++) {
+        swap(route[pos + 1], route[i]);
+        newLength = currentLength + nodeDistance(route[pos], route[pos + 1], x, y);
+        // skip if distance to next node already greater than our current best
+        if (newLength >= minCost) {
+            continue;
         }
+            minCost = min(minCost, travelSales(pos + 1, newLength, x, y, route, minCost));
+        
+        // undo swap
+        swap(route[pos + 1], route[i]);
     }
-    double distance = sqrt(pow(x[node] - x[minNode], 2) + pow(y[node] - y[minNode], 2));
-    return minCost + distance;
-}
+   }
+    return minCost;
+}   
 
 
 // Input: fin = pointer to ifstream object, fout = pointer to ofstream object
@@ -42,18 +49,25 @@ void solve(ifstream &fin, ofstream &fout) {
     fin >> n;
     vector<double> x; // holds x values of nodes
     vector<double> y; // holds y values of nodes
-
+    vector<int> route(n, 0);
+    // initial route
+    for (int i = 0; i < n; i++) {
+        route[i] = i;
+    }
     double xVal, yVal;
     while (n--) {
         fin >> xVal >> yVal;
         x.push_back(xVal);
         y.push_back(yVal);
     }
+    
 
-    vector<int> route;
-    int node = 0;
-    double cost = 0;
-    fout << travelSales(node, x, y, route) << endl;
+
+    
+    int pos = 0;
+    double minCost = DBL_MAX;
+    double currentLength = 0;
+    fout << travelSales(pos, currentLength, x, y, route, minCost) << endl;
     for (auto x : route) {
         fout << x << " ";
     }
